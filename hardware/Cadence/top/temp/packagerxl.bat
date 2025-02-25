209504d09220b44548ab8c1d004cd6c4096283ae
David Cussans <David.Cussans@bristol.ac.uk>
2025-02-25 10:50:50 +0000

    Fixed bizzare issue that one of the two side-launch LEDS (blue/red) got given the Ref Des MMC when exported to PCB. Fixed this by editing the RefDes in PCB and then doing physical-import back to schematic. Also re-printed circuit schematic

--- hardware/Cadence/top/temp/packagerxl.bat
+++ hardware/Cadence/top/temp/packagerxl.bat
@@ -1,4 +1,4 @@
 echo off
-netassembler
-concept2cm -forward -export -proj "C:\Users\phdgc\Downloads\uob-hep-pc072\hardware\Cadence\top\top_mib_v3.cpm"
-pxl.exe -proj "C:\Users\phdgc\Downloads\uob-hep-pc072\hardware\Cadence\top\top_mib_v3.cpm" -nosavehier
+rem PXL running in feedback mode
+rem netassembler
+pxl.exe -proj "C:\Users\phdgc\Downloads\uob-hep-pc072\hardware\Cadence\top\top_mib_v3.cpm" -nonetassembler  -rZ  
