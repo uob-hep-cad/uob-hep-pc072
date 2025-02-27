615aa256e2e9775505f525d7ed881e3ca073ce7e
David Cussans <David.Cussans@bristol.ac.uk>
2025-02-27 09:56:52 +0000

    Getting rid of files that are blocking PR from Pete

--- hardware/Cadence/top/temp/packagerxl.bat
+++
@@ -1,4 +0,0 @@
-echo off
-netassembler
-concept2cm -forward -export -proj "\\ppfs6.physics.ox.ac.uk\CEG\Central Electronics\Projects\Hastingsp\DUNE\uob-hep-pc072-new2\hardware\Cadence\top\mib_tongue2.cpm"
-pxl.exe -proj "\\ppfs6.physics.ox.ac.uk\CEG\Central Electronics\Projects\Hastingsp\DUNE\uob-hep-pc072-new2\hardware\Cadence\top\mib_tongue2.cpm" -nosavehier
