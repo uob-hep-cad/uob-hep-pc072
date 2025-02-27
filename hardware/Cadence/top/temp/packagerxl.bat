997b7bb21db85ca8e8dfe8609844d2283d30a305
David Cussans <David.Cussans@bristol.ac.uk>
2025-02-27 09:54:19 +0000

    Merged changes from Pete Hastings on tongue-2

--- hardware/Cadence/top/temp/packagerxl.bat
+++ hardware/Cadence/top/temp/packagerxl.bat
@@ -1,4 +1,4 @@
 echo off
-rem PXL running in feedback mode
-rem netassembler
-pxl.exe -proj "C:\Users\phdgc\Downloads\uob-hep-pc072\hardware\Cadence\top\top_mib_v3.cpm" -nonetassembler  -rZ  
+netassembler
+concept2cm -forward -export -proj "\\ppfs6.physics.ox.ac.uk\CEG\Central Electronics\Projects\Hastingsp\DUNE\uob-hep-pc072-new2\hardware\Cadence\top\mib_tongue2.cpm"
+pxl.exe -proj "\\ppfs6.physics.ox.ac.uk\CEG\Central Electronics\Projects\Hastingsp\DUNE\uob-hep-pc072-new2\hardware\Cadence\top\mib_tongue2.cpm" -nosavehier
