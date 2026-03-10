b12afad8b7b006aab62c22735107d01212db365b
David Cussans <David.Cussans@bristol.ac.uk>
2026-03-10 14:25:14 +0000

    Deleting journal/temp files that cause problems with merging...

--- hardware/Cadence/top/temp/packagerxl.bat
+++
@@ -1,4 +0,0 @@
-echo off
-netassembler
-concept2cm -forward -export -proj "\\ppfs6.physics.ox.ac.uk\CEG\Central Electronics\Projects\Hastingsp\DUNE\uob-hep-pc072-new2\hardware\Cadence\top\front_panel_jtag_serial.cpm"
-pxl.exe -proj "\\ppfs6.physics.ox.ac.uk\CEG\Central Electronics\Projects\Hastingsp\DUNE\uob-hep-pc072-new2\hardware\Cadence\top\front_panel_jtag_serial.cpm" -nosavehier
